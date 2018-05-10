40d3555b66cfab75315ed9363f14738c01d712c2
Vibhor Aggarwal <vibhorag24@gmail.com>
2018-05-10 12:53:29 +0200

    Update loadmodel.m

--- Matlab_models/ScriptforCalculation/loadmodel.m
+++ Matlab_models/ScriptforCalculation/loadmodel.m
@@ -1,12 +1,17 @@
 clear 
 clc
 
-Config.ON_GAZEBO = false;
+
+Config.ON_GAZEBO = false;%Set 'True', if working with Gazebo
 Config.Ts = 0.01;
-configjoint= 'l_ankle_pitch';
+configjoint= 'l_ankle_pitch';%The joint on which you want to run the experiment
+
+%%Setting the PID values
+
 %pids = WBToolbox.PIDConfiguration;
 %pids.addPID(WBToolbox.PID('r_ankle_pitch', 150, 0, 0));
 
+%%Running the model in a loop
 initdata;
 freq=initial:interval:final;
 n=fix((final-initial)/interval)+1;
@@ -16,8 +21,6 @@ clear interval;
 for i=1:(n)
    f=freq(i);
    sim('../SetPosition_2016')
-  
-   
 end
 clear i;
 
